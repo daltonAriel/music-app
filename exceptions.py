5f28589231bb2b90797b8a5352e8b12bf0f0c362
DESKTOP-1UOA0QM\PROGRAMMING <dalton_vargas@hotmail.com>
2023-09-13 22:07:19 -0600

    feat: Add custom exception handlers with response status

---
+++ exceptions.py
@@ -0,0 +1,61 @@
+from flask import Blueprint, jsonify
+from sqlalchemy.exc import IntegrityError, DataError
+from marshmallow.exceptions import ValidationError, MarshmallowError
+
+blueprintException = Blueprint("blueprintException", __name__)
+
+
+@blueprintException.app_errorhandler(IntegrityError)
+def integrityError(e: IntegrityError):
+    print(e)
+    return (
+        jsonify(
+            {
+                "message": "Database integrity error. A uniqueness constraint may have been violated.",
+                "exception": str(e.orig),
+            }
+        ),
+        500,
+    )
+
+
+@blueprintException.app_errorhandler(DataError)
+def dataError(e: DataError):
+    print(e)
+    return (
+        jsonify(
+            {
+                "message": "Database data error. The provided data is not valid.",
+                "exception": str(e),
+            }
+        ),
+        500,
+    )
+
+
+@blueprintException.app_errorhandler(ValidationError)
+def validationError(e: ValidationError):
+    print(e)
+    return (
+        jsonify(
+            {
+                "message": "Validation error. The provided data is not valid.",
+                "exception": str(e),
+            }
+        ),
+        400,
+    )
+
+
+@blueprintException.app_errorhandler(MarshmallowError)
+def marshmallowError(e: MarshmallowError):
+    print(e)
+    return (
+        jsonify(
+            {
+                "message": "An error occurred during data serialization/deserialization.",
+                "exception": str(e),
+            }
+        ),
+        500,
+    )
