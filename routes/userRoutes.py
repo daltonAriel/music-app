f7b4466ed808e3506a0109a2512264cfc331f865
DESKTOP-1UOA0QM\PROGRAMMING <dalton_vargas@hotmail.com>
2023-09-15 18:59:03 -0600

    fix: get credential from request body

--- routes/userRoutes.py
+++ routes/userRoutes.py
@@ -2,6 +2,8 @@ from flask import Blueprint, jsonify, request
 from data import UserRepository
 from models import UserSchema
 from utils import JwtUtils
+from flask_jwt_extended import jwt_required
+from utils import hasRole
 
 UserRoutes = Blueprint("UserRoutes", __name__)
 
@@ -11,25 +13,34 @@ def register():
     userRepository = UserRepository()
     schema = UserSchema()
     usuario = schema.load(request.get_json())
+
     if userRepository.coutByEmail(usuario.email) >= 1:
-        return jsonify({"message": "Registration failed, user alredy exist."}), 400
-    else:
-        user = userRepository.saveOne(usuario)
-        if user is not None:
-            jwtUtil = JwtUtils()
-            token = jwtUtil.createToken(user)
-            return jsonify({"token": token}), 201
-        else:
-            return jsonify({"message": "Registration failed"}), 400
+        return jsonify({"message": "Registration failed, user already exists."}), 400
 
+    user = userRepository.saveOne(usuario)
 
-@UserRoutes.route("/user/login", methods=["POST"])
-def login():
-    userRepository = UserRepository()
-    user = userRepository.getByCredentials(email="d4@gmail.coma", password="hola123")
     if user is not None:
         jwtUtil = JwtUtils()
         token = jwtUtil.createToken(user)
-        return jsonify({"token": token}), 200
-    else:
+        return jsonify({"token": token}), 201
+
+    return jsonify({"message": "Registration failed"}), 400
+
+
+@UserRoutes.route("/user/login", methods=["POST"])
+@hasRole(roles=['hola123', 'hola2'])
+def login():
+    email = request.json.get("email")
+    password = request.json.get("password")
+    userRepository = UserRepository()
+    user = userRepository.getByCredentials(email, password)
+
+    if user is None:
         return jsonify({"message": "Incorrect credentials"}), 401
+
+    if not user.active:
+        return jsonify({"message": "Error, account is not active"}), 403
+
+    jwtUtil = JwtUtils()
+    token = jwtUtil.createToken(user)
+    return jsonify({"token": token}), 200
