dbca33c431fb3b6bf312b2f79e4e5fa28712156c
DESKTOP-1UOA0QM\PROGRAMMING <dalton_vargas@hotmail.com>
2023-09-19 21:32:44 -0600

    feat: add refresh Token function

--- utils/jwt_utils.py
+++ utils/jwt_utils.py
@@ -7,10 +7,14 @@ class JwtUtils:
         token = create_access_token(
             identity=user.user_id,
             additional_claims={
-                "roles": user.roles,
+                "roles": [role.name for role in user.roles],
                 "first_name": user.first_name,
                 "last_name": user.last_name,
                 "email": user.email,
             },
         )
         return token
+
+    def createRefreshToken(self, identity):
+        freshToken = create_refresh_token(identity=identity)
+        return freshToken
