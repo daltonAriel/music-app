aea4f2b38e5ca2ee09304ae38be98aef1595c266
DESKTOP-SN9H7A8\ERDOGAN <dalton_vargas@hotmail.com>
2024-02-16 22:11:20 -0500

    feat: add custom validation with marshmallow

--- models/user.py
+++ models/user.py
@@ -1,13 +1,12 @@
-from extensions import db, ma, bc
+from extensions import db, ma
 from models.role import RoleSchema
-from marshmallow import validate
+from marshmallow import validate, fields, EXCLUDE
 
 
 class User(db.Model):
     __tablename__ = "users"
     user_id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(80), unique=False, nullable=False)
-    last_name = db.Column(db.String(80), unique=False, nullable=False)
+    user_name = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), unique=False, nullable=False)
     active = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -17,11 +16,31 @@ class User(db.Model):
 
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
+    user_name = ma.auto_field(
+        validate=[
+            validate.Length(max=80, error="Username must be less than 80 characters!")
+        ]
+    )
+    email = ma.auto_field(
+        validate=[
+            validate.Email(error="Invalid email format!"),
+            validate.Length(max=120, error="Email must be less than 120 characters"),
+        ]
+    )
+    password = ma.auto_field(
+        validate=[
+            validate.Length(
+                min=10, max=120, error="Password must be between 10 and 120 characters"
+            )
+        ]
+    )
     roles = ma.Nested(
         RoleSchema, many=True, exclude=["role_id", "description"], missing=[]
     )
+    active = fields.Boolean(missing=True)
 
     class Meta:
         model = User
+        unknown = EXCLUDE
         include_fk = False
         load_instance = True
