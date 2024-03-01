697636d8c587b718fe761adf4e2497951b49b53f
DESKTOP-SN9H7A8\ERDOGAN <dalton_vargas@hotmail.com>
2024-02-29 21:23:48 -0500

    fix: use createComponent and DOCUMENT to attach the modal

--- webapp/src/app/core/modal-base/service/ModalService.service.ts
+++ webapp/src/app/core/modal-base/service/ModalService.service.ts
@@ -1,24 +1,23 @@
-import { ComponentRef, Injectable, Injector, Type, ViewContainerRef, inject, createEnvironmentInjector, EnvironmentInjector } from '@angular/core';
+import { ComponentRef, Injectable, Injector, Type, inject, createEnvironmentInjector, EnvironmentInjector, createComponent, ApplicationRef } from '@angular/core';
 import { ModalBaseComponent } from "../modal-base.component";
 import { ModalOptionsI } from "../service/modalOptions";
 import { ModalContext } from './ModalContext';
+import { BackdropModalComponent } from '../backdrop-modal/backdrop-modal.component';
+import { DOCUMENT } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
 
-  private componentRefs: ComponentRef<ModalBaseComponent>[] = [];
+  private modalRefContainer: { modal: ComponentRef<any>, backDrop: ComponentRef<any> }[] = [];
   private readonly environmentInjector = inject(EnvironmentInjector);
-  private viewContainerRef = inject(ViewContainerRef);
+  private appRef = inject(ApplicationRef);
   private injector = inject(Injector);
+  private DOC = inject(DOCUMENT);
 
 
   openModal(content: Type<any>, customOptions: Partial<ModalOptionsI> = {}) {
-
-    const context = new ModalContext();
-    const env: EnvironmentInjector = createEnvironmentInjector([{ provide: ModalContext, useValue: context }], this.environmentInjector);
-
     const defaultOptions: ModalOptionsI = {
       size: 'md',
       scrollable: false,
@@ -26,46 +25,61 @@ export class ModalService {
       static: false,
     };
 
+    const context = new ModalContext();
+    const env: EnvironmentInjector = createEnvironmentInjector([{ provide: ModalContext, useValue: context }], this.environmentInjector);
     const modalOptions: ModalOptionsI = { ...defaultOptions, ...customOptions };
 
     return new Promise((resolve, reject): void => {
-      const modalBody = this.viewContainerRef.createComponent(content, { injector: this.injector, environmentInjector: env });
-      const childElementsArray:any = Array.from(modalBody.location.nativeElement.children);
+      const _body = createComponent(content, { elementInjector: this.injector, environmentInjector: env });
+      const modalBodyElements: any = Array.from(_body.location.nativeElement.children);
 
-      const modalContainer = this.viewContainerRef.createComponent(ModalBaseComponent, {
-        injector: this.injector, 
-        projectableNodes: [childElementsArray]
+      const modalBackdrop = createComponent(BackdropModalComponent, { elementInjector: this.injector, environmentInjector: this.appRef.injector });
+
+      const modalContainer = createComponent(ModalBaseComponent, {
+        elementInjector: this.injector,
+        environmentInjector: this.appRef.injector,
+        projectableNodes: [modalBodyElements]
       });
 
-
       modalContainer.instance.modalOptions = modalOptions;
+
+      this.appRef.attachView(modalContainer.hostView);
+      this.appRef.attachView(modalBackdrop.hostView);
+
       modalContainer.instance.closeModal = () => {
-        this.closeModal(modalContainer);
-        resolve(()=>{});
+        this.closeModal(modalContainer, modalBackdrop);
+        resolve(() => { });
       };
-      
-      context.close = () => {
-        modalContainer.instance.close()
+      modalContainer.instance.closeAllInstances = () => {
+        this.closeAll();
       }
 
+      context.close = () => {
+        this.closeModal(modalContainer, modalBackdrop);
+        resolve(() => { });
+      }
+      context.closeAll = () => {
+        this.closeAll();
+      }
+
+      this.DOC.body.appendChild(modalContainer.location.nativeElement);
+      this.DOC.body.appendChild(modalBackdrop.location.nativeElement);
+      this.modalRefContainer.push({ modal: modalContainer, backDrop: modalBackdrop });
     });
-
   }
 
-
-  getAllInstances(){
-    return this.componentRefs.map((ref)=>{
-      return ref.hostView;
-    });
-  
+  private closeModal(modalRef: ComponentRef<any>, backdropRef: ComponentRef<any>) {
+    let index = this.modalRefContainer.lastIndexOf({ backDrop: backdropRef, modal: modalRef });
+    this.modalRefContainer.splice(index, 1);
+    modalRef.destroy();
+    backdropRef.destroy();
   }
 
-
-  private closeModal(modal: ComponentRef<any>) {
-    let index = this.componentRefs.lastIndexOf(modal);
-    this.componentRefs.splice(index, 1);
-    modal.destroy();
+  closeAll() {
+    for (let i = this.modalRefContainer.length - 1; i >= 0; i--) {
+      this.modalRefContainer[i].modal.instance.closeModal();
+    }
+    this.modalRefContainer = [];
   }
-
 
 }
