1613643040572e743c34d095da1cb729a950a428
DESKTOP-SN9H7A8\ERDOGAN <dalton_vargas@hotmail.com>
2024-02-29 21:30:33 -0500

    fix: remove backdrop, remove fixexd styles, use :enter and :leave animations

--- webapp/src/app/core/modal-base/modal-base.component.ts
+++ webapp/src/app/core/modal-base/modal-base.component.ts
@@ -1,11 +1,16 @@
-import { Component, HostListener, Input, inject, OnDestroy, HostBinding, AfterViewInit, ElementRef, Renderer2 } from '@angular/core';
+import { Component, HostListener, Input, inject, OnDestroy, HostBinding, AfterViewInit, ElementRef, Renderer2, Inject, ChangeDetectorRef, AfterContentInit } from '@angular/core';
 import { DOCUMENT } from "@angular/common";
 import { CommonModule } from '@angular/common';
-import { ModalOptionsI } from "@core/modal-base/service/modalOptions";
-import { animate, state, style, transition, trigger, AnimationEvent } from '@angular/animations';
+import { ModalOptionsI } from "./service/modalOptions";
+import { animate, style, transition, trigger, AnimationEvent } from '@angular/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { config } from 'rxjs';
 
 
 @Component({
+  host: {
+    '[@modalAnimation]': 'true'
+  },
   selector: 'modal-base',
   standalone: true,
   imports: [CommonModule],
@@ -13,46 +18,60 @@ import { animate, state, style, transition, trigger, AnimationEvent } from '@ang
   styleUrls: ['./modal-base.component.scss'],
   animations: [
     trigger('modalAnimation', [
-      state('void', style({ transform: 'translateY(-25%)', opacity: 0 })),
-      transition('void => open', [
-        animate('0.3s ease-in-out', style({ transform: 'translateY(0%)', opacity: 1 }))
+      transition('void => *', [
+        style({ transform: 'translateY(-25%)', opacity: 0 }),
+        animate("0.3s ease-in-out", style({ transform: 'translateY(0%)', opacity: 1 }))
       ]),
-      transition('* => closed', [
-        animate('0.3s ease-in-out', style({ transform: 'translateY(-50%)', opacity: 0 }))
+      transition('* => void', [
+        animate("0.3s ease-in-out", style({ transform: 'translateY(-50%)' }))
       ]),
-      state('normal', style({ transform: 'scale(1)' })),
-      state('zoomed', style({ transform: 'scale(1.02)' })),
-      transition('* => zoomed', animate('0.2s ease-out')),
-      transition('* => normal', animate('0.2s ease-out')),
+      transition('* => zoomed', [
+        animate('0.2s ease-out', style({ transform: 'scale(1.02)' })),
+        animate('0.2s ease-out', style({ transform: 'scale(1)' })),
+      ])
     ])
   ],
+  styles: [`
+    :host {
+      display: block;
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      z-index: 1060;
+    }`]
 })
 export class ModalBaseComponent implements AfterViewInit, OnDestroy {
-
   @Input() modalOptions!: ModalOptionsI;
   @Input() closeModal!: () => void;
-  animationModal: 'open' | 'closed' | 'normal' | 'zoomed' = 'open';
+  @Input() closeAllInstances!: () => void;
+  animationModal: 'normal' | 'zoomed' = 'normal';
+
+  @HostBinding('@.disabled')
+  public animationsDisabled = false;
 
   private document = inject(DOCUMENT);
   private restoreOverflow = () => { };
   private el: ElementRef = inject(ElementRef);
   private render: Renderer2 = inject(Renderer2);
 
-  @HostBinding('@modalAnimation') public fadeInOut = true;
 
   @HostListener('document:keydown.escape', ['$event'])
   private handleEscapeKey(event: KeyboardEvent) {
     if (!this.modalOptions.static) {
-      this.close();
+      this.closeModal();
     } else if (this.modalOptions.static && this.modalOptions.size != 'fullScreen') {
       this.animationModal = 'zoomed';
     }
   }
 
+  constructor(private cdr: ChangeDetectorRef) { }
 
   ngAfterViewInit(): void {
     this.hideScroll();
     this.addClickOutsideListener();
+    this.cdr.detectChanges();
   }
 
 
@@ -65,8 +84,8 @@ export class ModalBaseComponent implements AfterViewInit, OnDestroy {
     const clickOutsideListener = (event: MouseEvent) => {
       if (!(event.target as HTMLElement).closest('.modalContent')) {
         if (!this.modalOptions.static) {
-          this.close();
-        } else if(this.modalOptions.static && this.modalOptions.size != 'fullScreen') {
+          this.closeModal();
+        } else if (this.modalOptions.static && this.modalOptions.size != 'fullScreen') {
           this.animationModal = 'zoomed';
         }
       }
@@ -98,15 +117,6 @@ export class ModalBaseComponent implements AfterViewInit, OnDestroy {
     if (event.toState === 'zoomed') {
       this.animationModal = 'normal';
     }
-    if (event.toState === 'closed') {
-      this.closeModal();
-    }
   }
-
-
-  close() {
-    this.animationModal = 'closed';
-  }
-
 
 } 
